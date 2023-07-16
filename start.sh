0163918dd7b4f1fe5c4934c91295fa5c43f6d071
pess0a <pessoa.felipe99@gmail.com>
2023-07-16 20:41:06 -0300

    a

--- start.sh
+++ start.sh
@@ -2,14 +2,24 @@
 
 if [ -d "logs" ]
 then
-	echo -e "\e[01;32m Starting server \e[0m"
+    echo -e "\e[01;32m Starting server with gdb enabled \e[0m"
 else
-	mkdir -p logs
-	sudo apt install -y gdb
+    mkdir -p logs
+    sudo apt install -y gdb
 fi
 
 ulimit -c unlimited
 set -o pipefail
 
-while true; do ./canary 2>&1 | awk '{ print strftime("%F %T - "), 
-$0; fflush(); }' | tee "logs/$(date +"%F %H-%M-%S.log")" done;
+while true; do gdb --batch -return-child-result --command=gdb_debug --args ./canary 2>&1 | awk '{ print strftime("%F %T - "), 
+$0; fflush(); }' | tee "logs/$(date +"%F %H-%M-%S.log")"
+
+if [ $? -eq 0 ]; then
+        echo -e "\e[0;31m Exit code 0, wait 10 seconds... \e[0m"
+        sleep 10
+    else
+        echo -e "\e[0;31m Restarting the server in 5 seconds "The log file is stored in the logs folder" \e[0m"
+        echo -e "\e[01;31m If you want to shut down the server, press CTRL + C... \e[0m"
+        sleep 10
+    fi
+done;
