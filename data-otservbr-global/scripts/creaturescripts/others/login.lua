9be2ecc9ecb7fb08e207675da949d53c87d01f4b
pess0a <pessoa.felipe99@gmail.com>
2023-07-16 20:27:45 -0300

    login

--- data-otservbr-global/scripts/creaturescripts/others/login.lua
+++ data-otservbr-global/scripts/creaturescripts/others/login.lua
@@ -21,18 +21,8 @@ end
 local playerLogin = CreatureEvent("PlayerLogin")
 
 function playerLogin.onLogin(player)
-	local items = {
-		{3003, 1},
-		{3031, 3}
-	}
 	if player:getLastLoginSaved() == 0 then
 		player:sendOutfitWindow()
-		local backpack = player:addItem(2854)
-		if backpack then
-			for i = 1, #items do
-				backpack:addItem(items[i][1], items[i][2])
-			end
-		end
 		player:addItem(2920, 1, true, 1, CONST_SLOT_AMMO)
 		db.query('UPDATE `players` SET `istutorial` = 0 where `id`='..player:getGuid())
 		-- Open channels
