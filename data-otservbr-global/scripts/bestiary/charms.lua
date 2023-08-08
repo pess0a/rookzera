5507106e67e266db16bde568eb22f7841400d97c
pess0a <pessoa.felipe99@gmail.com>
2023-08-08 19:53:26 -0300

    skill

--- data-otservbr-global/scripts/bestiary/charms.lua
+++ data-otservbr-global/scripts/bestiary/charms.lua
@@ -2,86 +2,86 @@ local charms = {
 	-- Wound charm
 	[1] = {
 		name = "Wound",
-		description = "Triggers on a creature with a certain chance and deals 5% \z
+		description = "Triggers on a creature with a certain chance and deals 10% \z
                        of its initial hit points as physical damage once.",
 		type = CHARM_OFFENSIVE,
 		damageType = COMBAT_PHYSICALDAMAGE,
-		percent = 5,
-		chance = 10,
+		percent = 10,
+		chance = 15,
 		messageCancel = "You wounded the monster.",
 		messageServerLog = "[Wound charm]",
 		effect = CONST_ME_HITAREA,
-		points = 600
+		points = 150
 	},
 	-- Enflame charm
 	[2] = {
 		name = "Enflame",
-		description = "Triggers on a creature with a certain chance and deals 5% \z
+		description = "Triggers on a creature with a certain chance and deals 10% \z
                        of its initial hit points as fire damage once.",
 		type = CHARM_OFFENSIVE,
 		damageType = COMBAT_FIREDAMAGE,
-		percent = 5,
-		chance = 10,
+		percent = 10,
+		chance = 15,
 		messageCancel = "You enflamed the monster.",
 		messageServerLog = "[Enflame charm]",
 		effect = CONST_ME_HITBYFIRE,
-		points = 1000
+		points = 160
 	},
 	-- Poison charm
 	[3] = {
 		name = "Poison",
-		description = "Triggers on a creature with a certain chance and deals 5% \z
+		description = "Triggers on a creature with a certain chance and deals 10% \z
                        of its initial hit points as earth damage once.",
 		type = CHARM_OFFENSIVE,
 		damageType = COMBAT_EARTHDAMAGE,
-		percent = 5,
-		chance = 10,
+		percent = 10,
+		chance = 15,
 		messageCancel = "You poisoned the monster.",
 		messageServerLog = "[Poison charm]",
 		effect = CONST_ME_GREEN_RINGS,
-		points = 600
+		points = 150
 	},
 	-- Freeze charm
 	[4] = {
 		name = "Freeze",
-		description = "Triggers on a creature with a certain chance and deals 5% \z
+		description = "Triggers on a creature with a certain chance and deals 10% \z
                        of its initial hit points as ice damage once.",
 		type = CHARM_OFFENSIVE,
 		damageType = COMBAT_ICEDAMAGE,
-		percent = 5,
-		chance = 10,
+		percent = 10,
+		chance = 15,
 		messageCancel = "You frozen the monster.",
 		messageServerLog = "[Freeze charm]",
 		effect = CONST_ME_ICEATTACK,
-		points = 800
+		points = 150
 	},
 	--Zap charm
 	[5] = {
 		name = "Zap",
-		description = "Triggers on a creature with a certain chance and deals 5% \z
+		description = "Triggers on a creature with a certain chance and deals 10% \z
                        of its initial hit points as energy damage once.",
 		type = CHARM_OFFENSIVE,
 		damageType = COMBAT_ENERGYDAMAGE,
-		percent = 5,
-		chance = 10,
+		percent = 10,
+		chance = 15,
 		messageCancel = "You eletrocuted the monster.",
 		messageServerLog = "[Zap charm]",
 		effect = CONST_ME_ENERGYHIT,
-		points = 800
+		points = 150
 	},
 	--Curse charm
 	[6] = {
 		name = "Curse",
-		description = "Triggers on a creature with a certain chance and deals 5% \z
+		description = "Triggers on a creature with a certain chance and deals 10% \z
                        of its initial hit points as death damage once.",
 		type = CHARM_OFFENSIVE,
 		damageType = COMBAT_DEATHDAMAGE,
-		percent = 5,
-		chance = 10,
+		percent = 10,
+		chance = 15,
 		messageCancel = "You curse the monster.",
 		messageServerLog = "[Curse charm]",
 		effect = CONST_ME_SMALLCLOUDS,
-		points = 900
+		points = 175
 	},
 	-- Cripple charm
 	[7] = {
@@ -90,7 +90,7 @@ local charms = {
 		type = CHARM_OFFENSIVE,
 		chance = 10,
 		messageCancel = "You cripple the monster.",
-		points = 500
+		points = 150
 	},
 	-- Parry charm
 	[8] = {
@@ -98,21 +98,21 @@ local charms = {
 		description = "Any damage taken is reflected to the aggressor with a certain chance.",
 		type = CHARM_DEFENSIVE,
 		damageType = COMBAT_PHYSICALDAMAGE,
-		chance = 10,
+		chance = 15,
 		messageCancel = "You parry the attack.",
 		messageServerLog = "[Parry charm]",
 		effect = CONST_ME_EXPLOSIONAREA,
-		points = 1000
+		points = 200
 	},
 	-- Dodge charm
 	[9] = {
 		name = "Dodge",
 		description = "Dodges an attack with a certain chance without taking any damage at all.",
 		type = CHARM_DEFENSIVE,
-		chance = 10,
+		chance = 15,
 		messageCancel = "You dodge the attack.",
 		effect = CONST_ME_POFF,
-		points = 600
+		points = 200
 	},
 	-- Adrenaline burst charm
 	[10] = {
@@ -122,16 +122,16 @@ local charms = {
 		type = CHARM_DEFENSIVE,
 		chance = 10,
 		messageCancel = "Your movements where bursted.",
-		points = 500
+		points = 100
 	},
 	-- Numb charm
 	[11] = {
 		name = "Numb",
 		description = "Numbs the creature with a certain chance after its attack and paralyzes the creature for 10 seconds.",
 		type = CHARM_DEFENSIVE,
-		chance = 10,
+		chance = 15,
 		messageCancel = "You numb the monster.",
-		points = 500
+		points = 150
 	},
 	-- Cleanse charm
 	[12] = {
@@ -141,64 +141,64 @@ local charms = {
 		type = CHARM_DEFENSIVE,
 		chance = 10,
 		messageCancel = "You purified the attack.",
-		points = 700
+		points = 125
 	},
 	-- Bless charm
 	[13] = {
 		name = "Bless",
-		description = "Blesses you and reduces skill and xp loss by 10% when killed by the chosen creature.",
+		description = "Blesses you and reduces skill and xp loss by 25% when killed by the chosen creature.",
 		type = CHARM_PASSIVE,
-		percent = 10,
+		percent = 25,
 		chance = 100,
-		points = 800
+		points = 100
 	},
 	-- Scavenge charm
 	[14] = {
 		name = "Scavenge",
 		description = "Enhances your chances to successfully skin/dust a skinnable/dustable creature.",
 		type = CHARM_PASSIVE,
-		percent = 25,
+		percent = 35,
 		chance = 100,
-		points = 800
+		points = 150
 	},
 	-- Gut charm
 	[15] = {
 		name = "Gut",
 		description = "Gutting the creature yields 20% more creature products.",
 		type = CHARM_PASSIVE,
-		percent = 20,
+		percent = 25,
 		chance = 100,
-		points = 800,
+		points = 150,
 	},
 	-- Low blow charm
 	[16] = {
 		name = "Low Blow",
-		description = "Adds 8% critical hit chance to attacks with critical hit weapons.",
+		description = "Adds 20% critical hit chance to attacks with critical hit weapons.",
 		type = CHARM_PASSIVE,
-		percent = 8,
+		percent = 20,
 		chance = 100,
-		points = 2000
+		points = 200
 	},
 	-- Divine wrath charm
 	[17] = {
 		name = "Divine Wrath",
-		description = "Triggers on a creature with a certain chance and deals 5% \z
+		description = "Triggers on a creature with a certain chance and deals 10% \z
                        of its initial hit points as holy damage once.",
 		type = CHARM_OFFENSIVE,
 		damageType = COMBAT_HOLYDAMAGE,
-		percent = 5,
-		chance = 10,
+		percent = 10,
+		chance = 15,
 		messageCancel = "You divine the monster.",
 		messageServerLog = "[Divine charm]",
 		effect = CONST_ME_HOLYDAMAGE,
-		points = 1500
+		points = 175
 	},
 	-- Vampiric embrace charm
 	[18] = {
 		name = "Vampiric Embrace",
-		description = "Adds 4% Life Leech to attacks if wearing equipment that provides life leech.",
+		description = "Adds 8% Life Leech to attacks if wearing equipment that provides life leech.",
 		type = CHARM_PASSIVE,
-		percent = 4,
+		percent = 8,
 		chance = 100,
 		points = 1500
 	},
@@ -209,7 +209,7 @@ local charms = {
 		type = CHARM_PASSIVE,
 		percent = 2,
 		chance = 100,
-		points = 1500
+		points = 130
 	}
 }
 
